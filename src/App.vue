cac451c1790b85630ee9248195a2ccc65bdcea7f
Matteo Sala <matteo111997@gmail.com>
2024-05-28 16:35:51 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import Header from './components/Header.vue'
+export default{
+  components: {
+    Header
+  }
+}
+</script>
+
+
+ <template>
+ <Header />
+</template>
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<script>
+export default{
+  name: 'Header'
+}
+</script>
+
+<template>
+  <div>
+    <img src="../assets/dog-taxi.png" alt="dog-taxi">
+  </div>
+</template>
+
+<style>
+
+</style>
